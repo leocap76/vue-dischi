724d589096dbdf71b30f66d1ffd9684cf78ca10f
Leonardo Capogna <leonardocapogna@gmail.com>
2020-12-17 16:57:00 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Esercizio:
+// Attraverso una chiamata ajax all’API di boolean
+// https://flynn.boolean.careers/exercises/api/array/music
+// avremo a disposizione una decina di dischi musicali.
+// Utilizzando vue, stampiamo a schermo una card per ogni album.
+// BONUS: Creare una select con tutti i generi dei dischi. In base a
+// cosa scegliamo nella select, vedremo i corrispondenti cd.
+// BONUS 2: Ordinare i dischi per anno di uscita.
+
+
+var app = new Vue({
+  el:'#root',
+  data: {
+    disc: []
+  },
+  methods:{
+
+  },
+  mounted: function(){
+    var self = this,
+
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/array/music'),
+    .then( function(result){
+      self.discs = result.data.response;
+      console.log(self.discs);
+     }
+    );
+  }
+});
